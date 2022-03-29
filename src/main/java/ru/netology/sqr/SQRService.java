524bd80719b7eed9d3b0e178e5c982999dca685f
Aleksandr16333 <97799611+Aleksandr16333@users.noreply.github.com>
2022-03-29 12:54:41 +0300

    Add files via upload

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -2,8 +2,6 @@ package ru.netology.sqr;
 
 public class SQRService {
     public int calculate(int min, int max) {
-
-
         int calc = 0;
         for (int i = 10; i <= 99; i++) {
             if (i * i >= min && i * i <= max) {
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,6 +1,5 @@
 package ru.netology.sqr;
 
-
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -9,10 +8,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class SQRServiceTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"'Test1', 200, 300, 3",
-            "'Test2', 400, 400, 1",
-            "'Test3', 100, 200, 5",
-            "'Test4', 300, 300, 0"})
+    @CsvSource(value = {"'calculating the internal limit', 200, 300, 3",
+            "'calculating the upper limit', 400, 400, 1",
+            "'calculating the lower limit', 100, 200, 5",
+            "'Calculating the upper limit', 300, 300, 0"})
     void calculate(String test, int min, int max, int expected) {
         SQRService service = new SQRService();
 
